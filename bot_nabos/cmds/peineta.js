749b9f34976280bfb55a7b23f72df5fe885d3fd8
Javier Barceló Pérez <javibarpe@hotmail.com>
2021-04-27 00:58:35 +0200

    added peineta >:(

---
+++ bot_nabos/cmds/peineta.js
@@ -0,0 +1,16 @@
+// CÓDIGO PARA EL COMANDO peineta
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+module.exports.run = async (client, message, args) => {
+    let peineta = "　    　  lﾆヽ\n　    　 |= | \n　    　 |_ |\n　　/⌒|~ |⌒i-、\n　 /|　|　|　| ｜\n　｜(　(　(　(　｜\n　｜　　　　　 ｜\n　 ＼　　　　　/\n　　 ＼　　　 |";
+    message.channel.send(peineta);
+}
+
+module.exports.config = {
+    name: "peineta",
+    description: "toma pa ti tonto",
+    usage: "/peineta",
+    accessableby: "Members",
+    aliases: ['p']
+}
