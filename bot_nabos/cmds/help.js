c0fd78dd36dd537571b7d7bb7f48fc109ab538b2
Javier Barceló Pérez <javibarpe@hotmail.com>
2021-04-27 00:17:19 +0200

    insultios

--- bot_nabos/cmds/help.js
+++ bot_nabos/cmds/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send("LISTA DE COMANDOS:\n    > /dado                                 Tira un dado de 6 caras.\n    > /moneda                            Tira una moneda para que salga cara o cruz.\n    > /patrullometro                 Da información sobre el patrullómetro\n    > /help                                   Muestra este menú\n    > /ping                                   pong!");
+    message.channel.send("LISTA DE COMANDOS:\n    > /dado \t\t\t\t Tira un dado del 1 al 6.\n> /jf \t\t\t\t tus muertos.\n    > /moneda \t\t\t\t Tira una moneda para que salga cara o cruz.\n    > /help \t\t\t\t Muestra este menú\n    > /ping \t\t\t\t pong!");
 }
 
 module.exports.config = {
